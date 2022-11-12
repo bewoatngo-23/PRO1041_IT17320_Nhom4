510ebbc043f8f2340d570400618bb15fb0aa0bd0
Ngô Vũ Ngọc Hào <ngovungochao14112003@gmail.com>
2022-11-12 08:49:12 +0700

    Khoi tao project

---
+++ demoNhom4/src/demonhom4/DemoNhom4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demonhom4;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DemoNhom4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("20");
+    }
+    
+}
