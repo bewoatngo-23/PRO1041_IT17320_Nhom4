ce25700f8e0f92b4a34807ad3cbd22d60e5e12b8
TrinhTrongVu <vutt15032003@gmail.com>
2022-11-12 09:01:03 +0700

    vu update

--- demoNhom4/src/demonhom4/DemoNhom4.java
+++ demoNhom4/src/demonhom4/DemoNhom4.java
@@ -14,7 +14,8 @@ public class DemoNhom4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("20");
+        System.out.println("20dfdf");
+        //adfdfd
     }
     
 }
